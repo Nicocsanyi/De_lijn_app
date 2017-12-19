4b573ef6128aace44328b82b130ea8adcf0f54d8
matthiaslybeert <matthias.l@hotmail.com>
2017-12-19 21:49:00 +0100

    root changes

--- Code/DeLijn_APP/routes/halten.js
+++ Code/DeLijn_APP/routes/halten.js
@@ -1,19 +1,3 @@
-var express = require("express");
-var root = express.Router();
-
-root.get('/', function(req, res) {
-  res.render("index", {
-       page_name: "index"
-  });
-});
-
-// Route naar halten
-root.get('/halten', function(req, res) {
-  res.render("halten", {
-       page_name: "halten"
-  });
-});
-
 var request = require('request')
 
 var halten = (req, res) => {
@@ -69,42 +53,3 @@ var getHaltes = resultaten => {
 
   return html
 }
-
-
-// Route naar home
-root.get('/home', function(req, res) {
-  res.render("home", {
-       page_name: "home"
-  });
-});
-
-// Route naar tickets
-root.get('/tickets', function(req, res) {
-  res.render("tickets", {
-       page_name: "tickets"
-  });
-});
-
-// Route naar haltes
-root.get('/haltes', function(req, res) {
-  res.render("haltes", {
-       page_name: "haltes"
-  });
-});
-
-// Route naar info
-root.get('/info', function(req, res) {
-  res.render("info", {
-       page_name: "info"
-  });
-});
-
-//verkooppunten
-root.get('/result', function(req, res) {
-  res.render("result", {
-       page_name: "result"
-  });
-});
-
-
-module.exports = root;
